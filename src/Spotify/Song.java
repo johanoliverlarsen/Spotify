d2ec7ce912e92b6b57ca3911b7e1c82a7cefe55d
lukasma02 <andersenlukas02@gmail.com>
2025-09-24 10:26:45 +0200

    new song class

--- src/Spotify/Song.java
+++ src/Spotify/Song.java
@@ -1,4 +1,36 @@
 package Spotify;
 
 public class Song {
+    private String title;
+    private Genre genre;
+
+
+    //Konstruktør
+    public song(String title, Genre genre){
+        this.title;
+        this.genre;
+    }
+    //Getters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    //Setters
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+    @Override
+    public String toString(){
+        return title + genre;
+    }
 }
