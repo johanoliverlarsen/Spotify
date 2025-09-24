2f16c25fc570912762b0dfec83d6ce0b85f9c85d
lindkvst <lindkviste@gmail.com>
2025-09-24 10:36:00 +0200

    Scanner

--- src/Spotify/ScannerHelper.java
+++ src/Spotify/ScannerHelper.java
@@ -1,4 +1,44 @@
 package Spotify;
 
+import java.util.Scanner;
+
 public class ScannerHelper {
+
+    private Scanner sc;
+
+    //Constructor
+
+    public ScannerHelper () {
+        this.sc = new Scanner(System.in);
+    }
+
+    public void welcomeMessage() {
+        System.out.println("Welcome!");
+    }
+
+    public int selectUser() {
+        boolean numCorrect = false;
+        int selectInt = 0;
+        int selectIntMax = 2;
+
+        if sc.hasNextInt() {
+            selectInt = sc.nextInt();
+            sc.nextLine();
+
+            if (selectInt < 0) {
+                System.out.println("You have entered a negative number. Try again");
+            } else if (selectInt == 0) {
+                System.out.println("You cannot enter 0. Try again");
+            } else if (selectInt > selectIntMax) {
+                System.out.println("You cannot enter numbers larger than " + selectIntMax + ". Try again");
+            }
+
+
+
+        }
+
+    }
+
+
+
 }
