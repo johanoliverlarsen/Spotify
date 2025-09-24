aa2c30a76e730a3b660d3cd07c1ca89001e8742c
lindkvst <lindkviste@gmail.com>
2025-09-24 11:12:16 +0200

    Scanner01

--- src/Spotify/ScannerHelper.java
+++ src/Spotify/ScannerHelper.java
@@ -21,24 +21,51 @@ public class ScannerHelper {
         int selectInt = 0;
         int selectIntMax = 2;
 
-        if sc.hasNextInt() {
-            selectInt = sc.nextInt();
-            sc.nextLine();
+        while (!numCorrect) {
 
-            if (selectInt < 0) {
-                System.out.println("You have entered a negative number. Try again");
-            } else if (selectInt == 0) {
-                System.out.println("You cannot enter 0. Try again");
-            } else if (selectInt > selectIntMax) {
-                System.out.println("You cannot enter numbers larger than " + selectIntMax + ". Try again");
+            if (sc.hasNextInt()) {
+                selectInt = sc.nextInt();
+                sc.nextLine();
+
+                if (selectInt < 0) {
+                    System.out.println("You have entered a negative number. Try again");
+                } else if (selectInt == 0) {
+                    System.out.println("You cannot enter 0. Try again");
+                } else if (selectInt > selectIntMax) {
+                    System.out.println("You cannot enter numbers larger than " + selectIntMax + ". Try again");
+                } else {
+                    numCorrect = true;
+                }
             }
-
-
-
         }
-
+        return selectInt;
     }
 
+    public int selectOption() {
+        boolean numCorrect = false;
+        int selectInt = 0;
+        int selectIntMax = 6;
 
+        while (!numCorrect) {
+
+            if sc.hasNextInt() {
+                selectInt = sc.nextInt();
+                sc.nextLine();
+
+                if (selectInt < 0) {
+                    System.out.println("You have entered a negative number. Try again");
+                } else if (selectInt == 0) {
+                    System.out.println("You cannot enter 0. Try again");
+                } else if (selectInt > selectIntMax) {
+                    System.out.println("You cannot enter numbers larger than " + selectIntMax + ". Try again");
+                } else {
+                    numCorrect = true;
+                }
+            }
+        }
+        return selectInt;
+    }
+
+ //   public
 
 }
